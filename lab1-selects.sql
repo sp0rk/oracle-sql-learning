e907ee3d6b718cff8be3d0adc5704bbaf3e6f333
sp0rk <julian.jurec@gmail.com>
2017-10-16 15:42:49 +0200

    Finished 1st set with refactor and tests

--- lab1-selects.sql
+++ lab1-selects.sql
@@ -1,6 +1,5 @@
 ALTER SESSION SET NLS_DATE_FORMAT='yyyy-mm-dd';
 
-
 --Zad. 1. Znajdź imiona wrogów, którzy dopuścili się incydentów w 2009r.
 SELECT 
     imie_wroga WROG,
@@ -57,7 +56,7 @@ SELECT
 FROM Kocury
 WHERE
   przydzial_myszy > 2 * NVL(myszy_extra, 0)
-  AND przydzial_myszy >= 55
+  AND przydzial_myszy >= 55;
 
 -- Zad. 8. Wyświetlić dla każdego kota (imię) następujące informacje o całkowitym rocznym spożyciu myszy: wartość
 -- całkowitego spożycia jeśli przekracza 660, ’Limit’ jeśli jest równe 660, ’Ponizej 660’ jeśli jest mniejsze od 660.
@@ -72,7 +71,7 @@ SELECT
     ELSE 'Ponizej 660'
   END 
     "Zjada rocznie"
-FROM Kocury
+FROM Kocury;
 
 -- Zad. 9. Po kilkumiesięcznym, spowodowanym kryzysem, zamrożeniu wydawania myszy Tygrys z dniem bieżącym wznowił
 -- wypłaty zgodnie z zasadą, że koty, które przystąpiły do stada w pierwszej połowie miesiąca (łącznie z 15-m)
@@ -89,13 +88,13 @@ SELECT
   CASE
     WHEN EXTRACT(DAY FROM w_stadku_od) <= 15 THEN
         CASE
-          WHEN NEXT_DAY(LAST_DAY('2017-10-23') - 7, 'ŚRODA') < '2017-10-23' THEN 
-              NEXT_DAY(LAST_DAY(ADD_MONTHS('2017-10-23', 1)) - 7, 'ŚRODA')
+          WHEN NEXT_DAY(LAST_DAY('2017-10-23') - 7, 'WEDNESDAY') < '2017-10-23' THEN 
+              NEXT_DAY(LAST_DAY(ADD_MONTHS('2017-10-23', 1)) - 7, 'WEDNESDAY')
           ELSE
-            NEXT_DAY(LAST_DAY('2017-10-23') - 7, 'ŚRODA')
+            NEXT_DAY(LAST_DAY('2017-10-23') - 7, 'WEDNESDAY')
         END
     ELSE
-      NEXT_DAY(LAST_DAY(ADD_MONTHS('2017-10-23', 1)) - 7, 'ŚRODA')
+      NEXT_DAY(LAST_DAY(ADD_MONTHS('2017-10-23', 1)) - 7, 'WEDNESDAY')
   END         
     "WYPLATA"
 FROM Kocury;
@@ -107,102 +106,103 @@ SELECT
   CASE
     WHEN EXTRACT(DAY FROM w_stadku_od) <= 15 THEN
         CASE
-          WHEN NEXT_DAY(LAST_DAY('2017-10-26') - 7, 'ŚRODA') < '2017-10-26' THEN 
-              NEXT_DAY(LAST_DAY(ADD_MONTHS('2017-10-26', 1)) - 7, 'ŚRODA')
+          WHEN NEXT_DAY(LAST_DAY('2017-10-26') - 7, 'WEDNESDAY') < '2017-10-26' THEN 
+              NEXT_DAY(LAST_DAY(ADD_MONTHS('2017-10-26', 1)) - 7, 'WEDNESDAY')
         ELSE
-          NEXT_DAY(LAST_DAY('2017-10-26') - 7, 'ŚRODA')
+          NEXT_DAY(LAST_DAY('2017-10-26') - 7, 'WEDNESDAY')
         END
     ELSE
-      NEXT_DAY(LAST_DAY(ADD_MONTHS('2017-10-26', 1)) - 7, 'ŚRODA')
+      NEXT_DAY(LAST_DAY(ADD_MONTHS('2017-10-26', 1)) - 7, 'WEDNESDAY')
   END
     "WYPLATA"
 FROM Kocury;
 
 -- Zad. 10. Atrybut pseudo w tabeli Kocury jest kluczem głównym tej tabeli. Sprawdzić, czy rzeczywiście wszystkie
 -- pseudonimy są wzajemnie różne. Zrobić to samo dla atrybutu szef.
-SELECT CASE COUNT(PSEUDO)
-       WHEN 1
-         THEN PSEUDO || ' - Unikalny'
-       ELSE PSEUDO || ' - nieunikalny'
-       END "Unikalnosc atr. PSEUDO"
-FROM KOCURY
-GROUP BY PSEUDO;
+SELECT 
+  CASE COUNT(pseudo)
+      WHEN 1 THEN 
+          pseudo || ' - Unikalny'
+      ELSE pseudo || ' - nieunikalny'
+      END "Unikalnosc atr. PSEUDO"
+FROM Kocury
+GROUP BY pseudo;
 
-SELECT CASE COUNT(SZEF)
-       WHEN 1
-         THEN SZEF || ' - Unikalny'
-       ELSE SZEF || ' - nieunikalny'
+SELECT CASE COUNT(szef)
+       WHEN 1 THEN 
+          szef || ' - Unikalny'
+       ELSE szef || ' - nieunikalny'
        END "Unikalnosc atr. SZEF"
-FROM KOCURY
-WHERE SZEF IS NOT NULL
-GROUP BY SZEF;
+FROM Kocury
+WHERE szef IS NOT NULL
+GROUP BY szef;
 
 -- Zad. 11. Znaleźć pseudonimy kotów posiadających co najmniej dwóch wrogów.
 SELECT
-  PSEUDO            "Pseudonim",
-  COUNT(IMIE_WROGA) "Liczba wrogow"
-FROM WROGOWIE_KOCUROW
-GROUP BY PSEUDO
-HAVING COUNT(IMIE_WROGA) >= 2;
+  pseudo "Pseudonim",
+  COUNT(imie_wroga) "Liczba wrogow"
+FROM Wrogowie_Kocurow
+GROUP BY pseudo
+HAVING COUNT(imie_wroga) >= 2;
 
 -- Zad. 12. Znaleźć maksymalny całkowity przydział myszy dla wszystkich grup funkcyjnych (z pominięciem SZEFUNIA
 -- i kotów płci męskiej) o średnim całkowitym przydziale (z uwzględnieniem dodatkowych przydziałów – myszy_extra)
 -- większym  od 50.
 SELECT
-  'Liczba kotow='                                                      " ",
-  COUNT(*)                                                             " ",
-  'lowi jako'                                                          " ",
-  FUNKCJA                                                              " ",
-  'i zjada max.'                                                       " ",
-  TO_CHAR(MAX(NVL(PRZYDZIAL_MYSZY, 0) + NVL(MYSZY_EXTRA, 0)), '90.00') " ",
-  'myszy miesiecznie'                                                  " "
-FROM KOCURY
+  'Liczba kotow=' " ",
+  COUNT(*) " ",
+  'lowi jako' " ",
+  funkcja " ",
+  'i zjada max.' " ",
+  TO_CHAR(MAX(NVL(przydzial_myszy, 0) + NVL(myszy_extra, 0)), '90.00') " ",
+  'myszy miesiecznie' " "
+FROM Kocury
 WHERE
-  FUNKCJA != 'SZEFUNIO'
-  AND PLEC != 'M'
-GROUP BY FUNKCJA
-HAVING AVG(NVL(PRZYDZIAL_MYSZY, 0) + NVL(MYSZY_EXTRA, 0)) > 50;
+  funkcja != 'SZEFUNIO'
+  AND plec != 'M'
+GROUP BY funkcja
+HAVING AVG(NVL(przydzial_myszy, 0) + NVL(myszy_extra, 0)) > 50;
 
 -- Zad. 13. Wyświetlić minimalny przydział myszy w każdej bandzie z podziałem na płcie.
 SELECT
-  NR_BANDY                     "Nr bandy",
-  PLEC                         "Plec",
-  MIN(NVL(PRZYDZIAL_MYSZY, 0)) "Minimalny przydzial"
-FROM KOCURY
-GROUP BY NR_BANDY, PLEC
+  nr_bandy "Nr bandy",
+  plec "Plec",
+  MIN(NVL(przydzial_myszy, 0)) "Minimalny przydzial"
+FROM Kocury
+GROUP BY nr_bandy, plec;
 
 -- Zad. 14. Wyświetlić informację o kocurach (płeć męska) posiadających w hierarchii przełożonych szefa pełniącego
 -- funkcję BANDZIOR (wyświetlić także dane tego przełożonego). Dane kotów podległych konkretnemu szefowi mają być
 -- wyświetlone zgodnie z ich miejscem w hierarchii podległości.
 SELECT
   LEVEL "Poziom",
-  PSEUDO "Pseudonim",
-  FUNKCJA "Funkcja",
-  NR_BANDY "Nr bandy"
-FROM KOCURY
-WHERE PLEC = 'M'
-CONNECT BY PRIOR PSEUDO = SZEF
-START WITH FUNKCJA = 'BANDZIOR';
+  pseudo "Pseudonim",
+  funkcja "Funkcja",
+  nr_bandy "Nr bandy"
+FROM Kocury
+WHERE plec = 'M'
+CONNECT BY PRIOR pseudo = szef
+START WITH funkcja = 'BANDZIOR';
 
 -- Zad. 15. Przedstawić informację o podległości kotów posiadających dodatkowy przydział myszy tak aby imię kota
 -- stojącego najwyżej w hierarchii było wyświetlone z najmniejszym wcięciem a pozostałe imiona z wcięciem odpowiednim
 -- do miejsca w hierarchii.
 SELECT
-  LPAD(LEVEL - 1, (LEVEL - 1) * 4 + 1, '===>') || '                ' || IMIE "Hierarchia",
-  NVL(SZEF, 'Sam sobie panem')                                               "Pseudo szefa",
-  FUNKCJA                                                                    "Funkcja"
-FROM KOCURY
-WHERE NVL(MYSZY_EXTRA, 0) > 0
-CONNECT BY PRIOR PSEUDO = SZEF
-START WITH SZEF IS NULL;
+  LPAD(LEVEL - 1, (LEVEL - 1) * 4 + 1, '===>') || '                ' || imie "Hierarchia",
+  NVL(szef, 'Sam sobie panem')                                               "Pseudo szefa",
+  funkcja                                                                    "Funkcja"
+FROM Kocury
+WHERE NVL(myszy_extra, 0) > 0
+CONNECT BY PRIOR pseudo = szef
+START WITH szef IS NULL;
 
 -- Zad. 16.  Wyświetlić określoną pseudonimami drogę służbową (przez wszystkich kolejnych przełożonych do głównego
 -- szefa) kotów płci męskiej o stażu dłuższym niż osiem lat (w poniższym rozwiązaniu datą bieżącą jest 11.07.2017) nie
 -- posiadających dodatkowego przydziału myszy.
-SELECT RPAD(' ', 4 * (LEVEL - 1)) || PSEUDO "Droga sluzbowa"
-FROM KOCURY
-CONNECT BY PRIOR SZEF = PSEUDO
+SELECT RPAD(' ', 4 * (LEVEL - 1)) || pseudo "Droga sluzbowa"
+FROM Kocury
+CONNECT BY PRIOR szef = pseudo
 START WITH
-  PLEC = 'M'
-  AND MONTHS_BETWEEN(SYSDATE, W_STADKU_OD) > 96
-  AND NVL(MYSZY_EXTRA, 0) = 0;
+  plec = 'M'
+  AND MONTHS_BETWEEN(SYSDATE, w_stadku_od) > 96
+  AND NVL(myszy_extra, 0) = 0;
